99fc21d09b072ae4233098bd238b6b8108c0d80a
Анастасия Оленёва <oleneva1995@icloud.com>
2021-10-25 08:25:58 +0300

    Добавьте файлы проекта.

---
+++ Lab 6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startString = Console.ReadLine();
+            string[] arrayString = startString.Split(' ', '.', ',', ';', ':', '-', '!', '?');
+            int max = 0;
+            foreach (string word in arrayString)
+            {
+                if (word.Length > max)
+                {
+                    max = word.Length;
+                }
+            }
+            Console.WriteLine("Самое длинное слово в предложении имеет {0} символов", max);
+            Console.ReadKey();
+
+
+        }
+    }
+}
